84cb5995b939f4e1771d9ea0de9c0bf1e2e033d6
valkovv7 <veliron94@gmail.com>
2026-04-09 10:44:49 +0300

    fmt files

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name = "StorageRG"
+    resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstoragevv2026"
-    container_name = "taskboardcontainer"
-    key = "terraform.tfstate"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
   }
 }
 
